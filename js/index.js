51400df2b64be1f413940fa0e8b7be01132b276f
qscesz1346 <qscesz1346@naver.com>
2022-06-21 14:32:15 +0900

    sss

---
+++ js/index.js
@@ -0,0 +1,24 @@
+const CIRCULATION_INTERVAL = 4000;
+
+let splashImages = window.document.body.querySelectorAll(':scope > section.splash > div');
+for(let i = 0; i < splashImages.length; i++) {
+    splashImages[i].style.backgroundImage = `url("../stylesheets/images/index-${i + 1}.jpg")`;
+    // 요소.style : 요소의 CSS 속성에 접근할 수 있게 한다.
+}
+
+// 재귀호출 (Recursive call) : 
+function circulateSplash(currentSplashIndex) {
+    setTimeout(() => {
+        splashImages[currentSplashIndex++].classList.remove('visible');
+        if (currentSplashIndex === splashImages.length) {
+            currentSplashIndex = 0;
+        }
+        splashImages[currentSplashIndex].classList.add('visible');
+        circulateSplash(currentSplashIndex);
+    }, CIRCULATION_INTERVAL);
+}
+circulateSplash(0);
+
+$('.enter').on('click',function(){
+    location.href="main.html";
+})
